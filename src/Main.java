e5c53fd170752841dabe4b2781469c05e3043186
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-12-11 20:13:05 -0300

    0.1.1

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,9 @@ public class Main {
             Scanner input = new Scanner(System.in);
 
             System.out.println("qual tipo de resultado vai escolher");
+
             System.out.print("soma digite 1, multiplicação digite 2, ");
+
             System.out.println("divisão digite 3, subtração digite 4.");
             int tipo = input.nextInt();
 
@@ -19,14 +21,25 @@ public class Main {
                     // Em construção...
                     break;
                 case 2:
+                    System.out.println("você escolheu a Multiplicação");
+                    // Em construção...
+                    break;
+                case 3:
+                    System.out.println("você escolheu a divisão");
+                    // Em construção...
+                    break;
+                case 4:
+                    System.out.println("você escolheu a Subtração");
+                    // Em construção...
+                    break;
+                default:
+                    System.out.println("essa opção não exister");
+                    break;
 
             }
 
         }
 
-
-
-
-
     }
+
 }
