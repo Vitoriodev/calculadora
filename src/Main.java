ccd0ffd48ee3afb49ddfbb0e98796608d7610428
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-12-12 14:58:48 -0300

    0.2.1

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,27 @@ public class Main {
             System.out.println("qual tipo de resultado vai escolher");
 
             System.out.print("soma digite 1, multiplicação digite 2, ");
-
             System.out.println("divisão digite 3, subtração digite 4.");
             int tipo = input.nextInt();
 
             switch (tipo){
                 case 1:
                     System.out.println("você escolheu a Soma");
+
+                    try{
+                        System.out.print("pode começar\n digite os numeros para soma.");
+                        int num1 = input.nextInt();
+                        int num2 = input.nextInt();
+                        int soma;
+
+                        soma = num1 + num2;
+                        System.out.println("primeiro numero "+num1+" segundo numero"+num2+" a soma é "+soma);
+
+                    } catch (Exception e){
+                        System.out.println("você não colocou um numero.");
+
+                    }
+
                     // Em construção...
                     break;
                 case 2:
@@ -37,6 +51,8 @@ public class Main {
                     break;
 
             }
+
+            // frecha o programa em construção...
 
         }
 
