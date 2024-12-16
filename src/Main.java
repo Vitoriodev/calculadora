e334ab66b035a528c0e9ff479f5e0c3922395137
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-12-16 14:19:26 -0300

    1.5

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,84 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static void Soma(Scanner input){
+
+        try{
+            System.out.print("pode começar\n digite os numeros para soma.");
+            float num1 = input.nextFloat();
+            float num2 = input.nextFloat();
+            float soma;
+
+            soma = num1 + num2;
+            System.out.println("primeiro numero "+num1+" segundo numero "+num2+" a soma é "+soma);
+
+        } catch (Exception e) {
+            System.out.println("você não colocou um numero.");
+            input.nextLine();
+
+        }
+    }
+
+    public static void Multiplicacao(Scanner input){
+
+
+        try{
+            System.out.print("pode começar\n digite os numeros para multiplicação.");
+            float num1 = input.nextFloat();
+            float num2 = input.nextFloat();
+            float multiplicacao;
+
+            multiplicacao = num1 * num2;
+            System.out.println("primeiro numero "+num1+" segundo numero "+num2+" a multiplicação é "+multiplicacao);
+
+        } catch (Exception e) {
+            System.out.println("você não colocou um numero.");
+            input.nextLine();
+        }
+    }
+
+    public static void Divisao(Scanner input){
+
+
+        try{
+            System.out.print("pode começar\n digite os numeros para divisão.");
+            float num1 = input.nextFloat();
+            float num2 = input.nextFloat();
+            float divisao;
+
+            divisao = num1 / num2;
+            System.out.println("primeiro numero "+num1+" segundo numero "+num2+" a divisão é "+divisao);
+
+        } catch (Exception e) {
+            System.out.println("você não colocou um numero.");
+            input.nextLine();
+        }
+    }
+
+    public static void Subtracao(Scanner input){
+
+        try{
+            System.out.print("pode começar\n digite os numeros para Subtração.");
+            float num1 = input.nextFloat();
+            float num2 = input.nextFloat();
+            float Subtracao;
+
+            Subtracao = num1 - num2;
+            System.out.println("primeiro numero " + num1 + " segundo numero " + num2 + " a Subtração é " + Subtracao);
+
+        } catch (Exception e) {
+            System.out.println("você não colocou um numero.");
+            input.nextLine();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Calculadora");
 
-        while (true){
+        Scanner input = new Scanner(System.in);
 
-            Scanner input = new Scanner(System.in);
+        while (true){
 
             System.out.println("qual tipo de resultado vai escolher");
 
@@ -14,37 +86,27 @@ public class Main {
             System.out.println("divisão digite 3, subtração digite 4.");
             int tipo = input.nextInt();
 
+
             switch (tipo){
                 case 1:
                     System.out.println("você escolheu a Soma");
+                    Soma(input);
 
-                    try{
-                        System.out.print("pode começar\n digite os numeros para soma.");
-                        int num1 = input.nextInt();
-                        int num2 = input.nextInt();
-                        int soma;
-
-                        soma = num1 + num2;
-                        System.out.println("primeiro numero "+num1+" segundo numero"+num2+" a soma é "+soma);
-
-                    } catch (Exception e){
-                        System.out.println("você não colocou um numero.");
-
-                    }
-
-                    // Em construção...
                     break;
                 case 2:
                     System.out.println("você escolheu a Multiplicação");
-                    // Em construção...
+                    Multiplicacao(input);
+
                     break;
                 case 3:
                     System.out.println("você escolheu a divisão");
-                    // Em construção...
+                    Divisao(input);
+
                     break;
                 case 4:
                     System.out.println("você escolheu a Subtração");
-                    // Em construção...
+                    Subtracao(input);
+
                     break;
                 default:
                     System.out.println("essa opção não exister");
@@ -52,7 +114,13 @@ public class Main {
 
             }
 
-            // frecha o programa em construção...
+            System.out.print("Você quer fechar a calculadora? (1) para sim (2) para não: ");
+            int fechar = input.nextInt();
+
+            if(fechar == 1){
+                System.out.println("ESTAMOS FECHANDO O PROGRAMA!");
+                break;
+            }
 
         }
 
